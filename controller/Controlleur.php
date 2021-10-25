a3e9dba4940c72670b9c48cfd201316d75da67ef
lucf11 <lucas11ferrier@gmail.com>
2021-10-25 22:03:09 +0200

    modelEtudiant en cours

--- controller/Controlleur.php
+++ controller/Controlleur.php
@@ -3,6 +3,12 @@
 require_once '../model/Model.php';
 
 class Controlleur{
-    
+    public static function readAll(){
+        require('../view/list.php');
+    }
+
+    public static function read(){
+        require('../view/detail.php');
+    }
 }
 ?>
---
+++ view/list.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage Dossier</title>
+</head>
+<body>
+    <?php
+    /// affichage des dossiers de tous les étudiants.
+    ?>
+</body>
+</html>
