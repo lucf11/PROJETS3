147882a8654dc4fdedf3b262629bddfe83cb544f
pierre-fonzes <78018287+pierre-fonzes@users.noreply.github.com>
2021-11-28 16:52:27 +0100

    ReadAllAggreg à jour
    Implémentations des méthodes nécessaires pour l'action du readAllAggreg comme redéfinit(avec aggreg d'aggregs possible et coefficient déf par Messaoui)

--- model/ModelModule.php
+++ model/ModelModule.php
@@ -16,12 +16,28 @@ class ModelModule {
     public function getnomModule() {
         return $this->nomModule;
     }
-    public function getcoefModule(){
+    public function getCoeff(){
+        if($this->coefModule==null){//voir méthode calculerNoteV2 pour comprendre
+            require_once "Model.php";
+            $sql = "SELECT coefModule from projets3_module WHERE idModule='{$this->idModule}'";
+            // Préparation de la requête
+            try{
+                $req_prep = Model::getPDO()->prepare($sql);
+                // On donne les valeurs et on exécute la requête     
+                $req_prep->execute();
+                // On récupère les résultats comme précédemment
+                $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelAggreg');
+                $coeff = $req_prep->fetchAll();
+                return $coeff[0][0]; 
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }   
+        }
         return $this->coefModule;
     }
     public static function getModulebyID($id) {
         require_once "Model.php";
-        $sql = "SELECT * from Module WHERE idModule=:nom_tag";
+        $sql = "SELECT * from projets3_module WHERE idModule=:nom_tag";
         // Préparation de la requête
         try{
             $req_prep = Model::getPDO()->prepare($sql);
@@ -47,7 +63,7 @@ class ModelModule {
     public static function getAllModules(){
         require_once('Model.php');
         try{
-            $rep = Model::getPDO()->query('SELECT * FROM Modules');
+            $rep = Model::getPDO()->query('SELECT * FROM projets3_module');
             $rep->setFetchMode(PDO::FETCH_CLASS, 'modelModule');
             $tab_mods = $rep->fetchAll();      
         }
@@ -56,11 +72,12 @@ class ModelModule {
         }
         return $tab_mods;
     }
-    public function getNoteBycodeNIP($id, $idmodule){
+    public function getNoteBycodeNIP($id){
+        //var_dump($id);
         require_once "Model.php";
         //var_dump($this->idModule);
         //var_dump($id);
-        $sql = "SELECT noteModule from notemodule WHERE codeNIP=:nom_tag AND idModule='{$idModule}'";//sélection de la note de l'étudiant sur le module correspondant
+        $sql = "SELECT noteModule from projets3_notemodule WHERE codeNIP=:nom_tag AND idModule='{$this->idModule}'";//sélection de la note de l'étudiant sur le module correspondant
         // Préparation de la requête
         try{
             $req_prep = Model::getPDO()->prepare($sql);
@@ -78,15 +95,15 @@ class ModelModule {
         }catch(PDOException $e){
             echo $e->getMessage();
         }
-        // Attention, si il n'y a pas de résultats, on renvoie false
+        // Attention, si il n'y a pas de résultats, cela veut dire qu'on travaille avec des aggregs
         if (empty($tab_mod))
             return false;
-        return $tab_mod[0];//on retourne la liste de tous les modules qui sont dans l'idAggreg passsé en param
+        return (int)$tab_mod[0][0];//on retourne la liste de tous les modules qui sont dans l'idAggreg passsé en param
     }
     
     public static function getAggregbyID($idAggreg){
         require_once "Model.php";
-        $sql = "SELECT idModule from listemodulesaggreg WHERE idAggreg=:nom_tag";
+        $sql = "SELECT idModule from projets3_listemoduleagreger WHERE idAgregation=:nom_tag";
         // Préparation de la requête
         try{
             $req_prep = Model::getPDO()->prepare($sql);
@@ -102,11 +119,14 @@ class ModelModule {
             $req_prep->setFetchMode(PDO::FETCH_CLASS, 'ModelModule');
             $tab_mod = $req_prep->fetchAll();
         }catch(PDOException $e){
-            echo $e->getMessage();
+            echo 'erreur';
         }
-        // Attention, si il n'y a pas de résultats, on renvoie false
-        if (empty($tab_mod))
-            return false;
+        // Attention, si un idModule est null -> ça veut dire qu'on est sur une aggreg d'aggreg
+        foreach($tab_mod as $value){
+            if($value->getidModule()==null){
+                return false;
+            }
+        }
         return $tab_mod;//on retourne la liste de tous les modules qui sont dans l'idAggreg passsé en param
     }
 
